9ee26b79509a496de1502ad12da26e5223c6af5f
Tom Aarts <taarts1@gmail.com>
2023-08-11 19:30:37 -0500

    mcp updates and modified dir structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,20 @@
-module Pi4J.V2.Devices.Testing {
+module com.pi4j.test.devices {
     requires com.pi4j;
     //requires com.pi4j.devices;
 
     //requires com.pi4j.plugin.pigpio;
 
     // SLF4J MODULES   LOG4J
-    // requires org.slf4j;
+   // requires org.slf4j;
     //requires org.apache.logging.log4j;
-    // requires org.apache.logging.log4j.core;
+   // requires org.apache.logging.log4j.core;
 
 
-    // requires jdk.unsupported;
+   // requires jdk.unsupported;
     //requires com.pi4j.plugin.linuxfs;
     requires com.pi4j.devices;
     requires jdk.unsupported;
     requires com.pi4j.plugin.linuxfs;
+
+
 }
