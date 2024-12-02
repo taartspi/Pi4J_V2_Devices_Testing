0d83927aad13d2d86e77d4c47de984ff547f195e
Tom Aarts <taarts1@gmail.com>
2024-12-01 20:59:33 -0600

    cleanup

--- src/main/java/com/pi4j/test/devices/dht22/Pi4jTest.java
+++ src/main/java/com/pi4j/test/devices/dht22/Pi4jTest.java
@@ -10,8 +10,6 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
